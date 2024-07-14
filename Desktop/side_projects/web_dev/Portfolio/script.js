1334cdeb3d293469541a7a5a38dc8bcc95421b0e
Grace Ozog <gozogg@umich.edu>
2024-07-14 19:46:04 -0400

    first commit

---
+++ Desktop/side_projects/web_dev/Portfolio/script.js
@@ -0,0 +1,13 @@
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function changeBorder(element) {
+    element.style.borderWidth = "20px";
+}
+
+function resetBorder(element) {
+    element.style.borderWidth = "5px";
+}
